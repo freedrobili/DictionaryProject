bfefb839a46f7cdfd4f53ac0cebc9c7d8dbfb4d3
Yuliya Kudashkina <kudashkina-yuliya@mail.ru>
2023-08-25 13:03:16 +0300

    Initial commit

---
+++ src/main/java/com/demo/DictionaryProject/DictionaryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.DictionaryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryProjectApplication.class, args);
+	}
+
+}
