aeae39de59b2f5ff592a234e9b24350f21131bd6
Yuliya Kudashkina <kudashkina-yuliya@mail.ru>
2023-08-28 16:53:26 +0300

    v 1.0.3

--- src/main/java/com/demo/DictionaryProject/service/CategoryService.java
+++ src/main/java/com/demo/DictionaryProject/service/CategoryService.java
@@ -20,36 +20,6 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
-
-//    private List<Category> categories = List.of(
-//            new Category(5815, "Сервис для покупки медиа-контента (книги, фильмы, музыка)", true, null,
-//                    "Вам необходимо предоставить документы, подтверждающие, что контент лицензионный, и нет нарушения авторских прав."),
-//            new Category(5661, "Обувные магазины", true, "На сайте присутствуют товары известных брендов.",
-//                    "Вам необходимо предоставить документы, подтверждающие подлинность товаров (договор с официальным дистрибьютором или товарные накладные).")
-//    );
-//
-//    public List<Category> getCategoriesByProduct(String productName) {
-//        return categories;
-//    }
-//
-//    public ShortCategories getShortCategoriesByProduct(String productName) {
-//        List<ShortCategoriesResponse> shortCategoriesResponses = categories.stream()
-//                .map(category -> {
-//                    ShortCategoriesResponse response = new ShortCategoriesResponse();
-//                    response.setCode(String.valueOf(category.getCode()));
-//                    response.setName(category.getName());
-//                    response.setNeedsDocs(category.isNeedsDocs());
-//                    response.setNeedsDocsReason(category.getNeedsDocsReason());
-//                    response.setNeedsDocsList(category.getNeedsDocsList());
-//                    return response;
-//                })
-//                .collect(Collectors.toList());
-//
-//        ShortCategories shortCategories = new ShortCategories();
-//        shortCategories.setCategories(shortCategoriesResponses);
-//
-//        return shortCategories;
-//    }
     public List<Category> getCategoriesByProduct(String productName) {
         return categoryRepository.findByProduct(productName);
 }
