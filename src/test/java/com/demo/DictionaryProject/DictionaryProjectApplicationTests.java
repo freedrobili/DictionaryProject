001e2aa5b3085c4a54d1eaff8ddc438472972c75
Yuliya Kudashkina <kudashkina-yuliya@mail.ru>
2023-08-29 14:00:10 +0300

    v 1.0.5

--- src/test/java/com/demo/DictionaryProject/DictionaryProjectApplicationTests.java
+++ src/test/java/com/demo/DictionaryProject/DictionaryProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DictionaryProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
